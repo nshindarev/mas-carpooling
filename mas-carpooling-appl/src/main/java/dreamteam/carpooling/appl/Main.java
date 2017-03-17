89c7cf55e11f8addeeaec1dcb79a01d244509e50
Shindarev Nikita <nshindarev@gmail.com>
2017-03-17 21:17:40 +0300

    Cоздан application-модуль проекта. Создана точка входа проекта.

---
+++ mas-carpooling-appl/src/main/java/dreamteam/carpooling/appl/Main.java
@@ -0,0 +1,10 @@
+package dreamteam.carpooling.appl;
+
+/**
+ * Created by nshindarev on 17.03.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Application started.");
+    }
+}
