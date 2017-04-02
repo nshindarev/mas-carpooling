c2aa2c5dfe0d2327af00cc74aa0d3c2a4937b8e8
GBagretsov <gbagretsov@gmail.com>
2017-04-02 16:56:14 +0300

    Добавлены заглушки поведений для ролей водителя и пассажира: - для водителя - регистрация в сервисе Yellow Pages и обработка запросов от пассажиров - для пассажира - обработка запросов от водителей

--- mas-carpooling-appl/src/main/java/dreamteam/carpooling/appl/CitizenAgent.java
+++ mas-carpooling-appl/src/main/java/dreamteam/carpooling/appl/CitizenAgent.java
@@ -1,19 +1,31 @@
 package dreamteam.carpooling.appl;
 
+import dreamteam.carpooling.appl.DriverBehaviours.HandlePassengersOffersBehaviour;
+import dreamteam.carpooling.appl.DriverBehaviours.RegisterInYPBehaviour;
+import dreamteam.carpooling.appl.PassengerBehaviours.HandleDriversOffersBehaviour;
+
 import jade.core.Agent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Created by Георгий on 02.04.2017.
+ * Агент - житель города. Каждый агент может играть две роли: пассажира и водителя.
+ * В какой-то момент он должен определиться, кем он будет в итоге.
  */
 public class CitizenAgent extends Agent {
 
-    private static final Logger logger = LoggerFactory.getLogger(CitizenAgent.class);
+    public static final Logger logger = LoggerFactory.getLogger(CitizenAgent.class);
 
     @Override
     protected void setup() {
         super.setup();
         logger.info("Hello! Agent {} is ready.", getAID().getName());
+
+        // Поведения для роли водителя
+        addBehaviour(new RegisterInYPBehaviour());
+        addBehaviour(new HandlePassengersOffersBehaviour());
+
+        // Поведения для роли пассажира
+        addBehaviour(new HandleDriversOffersBehaviour());
     }
 }
